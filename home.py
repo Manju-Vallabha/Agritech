44ef6723c8f64b04c7188cb9cce733c6e30bb2f6
Manju Vallabha Pavalla <143684911+Manju-Vallabha@users.noreply.github.com>
2024-03-17 20:37:08 +0530

    Add files via upload

---
+++ home.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+def app():
+    st.title('Home')
+    st.write('Welcome to the home page of this site. This is a simple site to demonstrate how to use Streamlit to create web applications. The site is divided into three sections: Home, Data, and Model. You can navigate to each section by clicking on the links in the sidebar.')
+
+app()
